2ddf2eb9a28ce8ae1a3b813874c1063996fe29ae
Ritu <ritutec001@gmail.com>
2023-02-16 11:05:59 +0530

    add file

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className="messageInfo">
         <img src="logo.jpeg" alt="" />
         <span>just now</span>
       </div>
-      <div className="mesasgeContent">
-        <span>hello ! i am using waht's app</span>
-        {/* <img src="logo.jpeg" alt="" /> */}
+      <div className="messageContent">
+        <p>hello ! i am using waht's app</p>
+        <img src="logo.jpeg" alt="" />
       </div>
     </div>
   )
